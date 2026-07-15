80a1074f39aa8245e02e71f9093527a5c292d02d
Victor Augusto <victoraugustogoncalvesvalerio@gmail.com>
2026-07-15 17:26:51 -0300

    feat: route /admin/users/filter

--- Backend/src/routes/adminRoutes.js
+++ Backend/src/routes/adminRoutes.js
@@ -5,7 +5,8 @@ import { getAllUsersController , GetAllUsersFilter} from '../controllers/adminCo
 const router = express.Router();
 
 
-
+router.use(authMiddleware);
+router.use(adminOnly)
 
 router.get('/users', getAllUsersController);
 router.get('/users/filter', GetAllUsersFilter);
