65b7c22c091ebefe10d12c76ade2f95e91d64683
Davghost <acrookedfork@gmail.com>
2026-07-16 14:05:40 -0300

    Feat: excluded all updates about webpush firebase

--- Backend/src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../../pdifire.json'); // o .json baixado
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -4,7 +4,6 @@ import PDIList from '../pages/PDIList.vue'
 import PDIRegister from '../pages/PDIRegister.vue'
 import PDIUpdate from '../pages/PDIUpdate.vue'
 import { clearSession, getCurrentUser, isAuthenticated } from '../services/api'
-import { registrarPush } from "@/firebase"
 
 const routes = [
   { path: '/', redirect: '/login' },
@@ -41,13 +40,6 @@ router.beforeEach(async (to) => {
   }
 
   if (to.meta.role && user?.role !== to.meta.role) return home
-
-  // AQUI ⬇️ registra o push uma única vez, quando detecta usuário autenticado
-  if (user && isAuthenticated() && !pushJaRegistrado) {
-    pushJaRegistrado = true
-    const token = localStorage.getItem('authToken') // ou o nome que você usa no services/api
-    registrarPush(import.meta.env.VITE_API_URL, token) // sem await, não trava a navegação
-  }
 })
 
 export default router
